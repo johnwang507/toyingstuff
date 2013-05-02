763f246d9372a97a5ff0b2343b354daa261ca5e9
johnwang507 <john.wang507@gmail.com>
2013-05-02 20:58:52 +0800

    add support to MSAccess

--- dataloader.py
+++ dataloader.py
@@ -1,44 +1,59 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-# A not-so-goood data load utility created by John Wang(john.wang507@gmail.com) in Oct/2012
+# A not-so-goood data load utility created by John Wang(john_wang07@infosys.com) in Oct/2012
 #
 
-import argparse, os, sys, glob, datetime, csv, re, traceback, sqlite3, codecs
-import cx_Oracle as oracle
+import argparse, os, sys, glob, datetime, csv, re, traceback, codecs
+
+SUPPORTED_DBS = ['oracle','sqlite3','msaccess']
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Load data from CSVs to a database.',
+    parser = argparse.ArgumentParser(description='A tool loading data from a set of CSV files into database. Created by John Wang(john_wang07@infosys.com)',
                             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     def _exit_w_info(info):
         print '\n%s\n' % info
         parser.print_help()
         sys.exit(0)
-    parser.add_argument('-m','--mapping', default="mapping.csv",
-                   help="The path of mapping configure file.")
-    parser.add_argument('-f','--file_path',help="The data file path from where the data been loaded.")
-    parser.add_argument('-e','--execute', action="store_true", 
+    
+    db_opts = parser.add_argument_group('Database Options')
+    db_opts.add_argument('-l','--db_dialect', choices=SUPPORTED_DBS,
+                   default='sqlite3', 
+                   help="The target database engine.")
+    db_opts.add_argument('-d','--db_host', default='localhost', help="The target database server address. If a file database engine(e.g., sqlite3 or msaccess) is used, the value should be the path to the file.")
+    db_opts.add_argument('-o','--db_port',type=int, default=1521, help="The port the target database server is listening on. If sqlite_db is specified, this option will be ignored.")
+    db_opts.add_argument('-s','--db_sid', help="The database server sid. If sqlite_db is specified, this option will be ignored.")
+    db_opts.add_argument('-u','--db_user',help="The user name used to connect to the database server. If sqlite_db is specified, this option will be ignored.")
+    db_opts.add_argument('-p','--db_pwd',help="The user password used to connect to the database server. If sqlite_db is specified, this option will be ignored.")
+    db_opts.add_argument('-e','--db_encoding', default='utf8', help="The encoding the DB driver expects for the value to be inserted.")
+    
+    mp_opts = parser.add_argument_group('Mapping Configure Options')
+    mp_opts.add_argument('-m','--mapping', default="mapping.csv",
+                   help="The path of the mapping configure file.")
+    mp_opts.add_argument('-f','--file_path',help="The data file path from where the data been loaded.")
+    mp_opts.add_argument('-w','--raw_date_fmt', default='%m/%d/%Y %I:%M:%S %p', help='The date format used to parse the source data of datetime type from the CSVs. You need to quote the parameter if it has spaces in it.')
+    mp_opts.add_argument('csvs', metavar='csv', nargs='*',help="The CSV files to be loaded into the Database.")
+    
+    exe_opts = parser.add_argument_group('Execute Options')
+    exe_opts.add_argument('-x','--execute', action="store_true", 
                    help="Execute the SQL in the database instead of print them out.")
-    parser.add_argument('-t','--create_table', action="store_true", 
-                   help="If provided, the table will be created before insert data.")
-    parser.add_argument('-v','--verbose', action="store_true", help='If present, a lot of process information will be output on screen.')
-    parser.add_argument('-l','--sqlite_db',
-                   help="The sqlite database file to use. If not specified, oracle will be used.")
-    parser.add_argument('-d','--db_host', default='localhost', help="The database host address.")
-    parser.add_argument('-o','--db_port',type=int, default=1521, help="The database host port.")
-    parser.add_argument('-s','--db_sid', help="The database server sid")
-    parser.add_argument('-u','--db_user',help="The user name used to connect to the database server")
-    parser.add_argument('-p','--db_pwd',help="The user password used to connect to the database server")
-    parser.add_argument('-w','--raw_date_fmt', default='%m/%d/%Y %I:%M:%S %p', help='The date format used to parse the data of datetime type from the source. You need to quote the parameter if it has spaces in it.')
-    parser.add_argument('-b','--pk_base', type=int, default=1000, help="The starting value of generated primary keys for a database record.")
-    parser.add_argument('csvs', metavar='csv', nargs='*',help="The CSV files to be loaded into oracle DB.")
+    exe_opts.add_argument('-t','--create_table', action="store_true", 
+                   help="If provided, the table will be created before the records being inserted.")
+    exe_opts.add_argument('-a','--batch_insert', action="store_true", help='If present, inserting to table will be executed in batch mode.')
+    exe_opts.add_argument('-v','--verbose', action="store_true", help='If present, detailed processing(log) information will be shown on the screen.')
+    exe_opts.add_argument('-k','--pk_base', type=int, default=1000, help="The starting value of generated primary keys for a database record.")
+    
+    
+    
     args = parser.parse_args()
-    if args.execute and not args.sqlite_db and not all((args.db_user, args.db_pwd, args.db_sid)):
-        missings = filter(lambda x:not x[1],(('-u',args.db_user), ('-p',args.db_pwd), ('-s',args.db_sid)))
-        _exit_w_info('Error: Option(s) missing: %s.' % ', '.join([o for o,_ in missings]))
     if args.file_path:
         args.csvs.extend(glob.glob(args.file_path))
     if not os.path.isfile(args.mapping):
         _exit_w_info('Error: mapping configure not exists: "%s". You can specify a valid mapping file by the "-m" option' % args.mapping)
+    if args.db_dialect == SUPPORTED_DBS[2]: # Validating MS Access database options
+        if os.path.isfile(args.db_host):
+            args.db_host = os.path.abspath(args.db_host) # Seemed like MS Access only support absolute path.
+        else:
+            _exit_w_info('Error: MS Access Database file "%s" not found .' % args.db_host)
     if not args.csvs:
         _exit_w_info('Error: no data(csv) file found. Please specify valid CSV files either by the "-f" option or the positional argument.')
     print 'Commandline args:\n', vars(args)
@@ -87,14 +102,14 @@ def load_csv(fpath,mapping):
     fname, _, ext=os.path.basename(fpath).rpartition('.')
     fname = (fname or ext).lower()
     rt = None
-    if fname in [mp['_s_name'] for mp in mapping]:
+    if fname in [mp[1] for mp in mapping]:
         with open(fpath,'rb') as f:
             reader = UnicodeReader(f)
             headers = [h.lower() for h in reader.next()]
             rows = [zip(headers,[c.strip() for c in row]) for row in reader if ''.join(row).strip()]
             if args.verbose: print 'raw csv data:\n', rows[:2]
-            used_fields = set(sum([[fmp[3].partition(':')[0] for k, fmp in mp.items() if k not in ('_s_name', '_t_name')]
-                                    for mp in mapping if mp['_s_name']==fname], []))
+            used_fields = set(sum([[fmp[3].partition(':')[0] for k, fmp in mp[2:]]
+                                    for mp in mapping if mp[1]==fname], []))
             if args.verbose: print 'used fields:', used_fields
             rt = [[(k,v) for k,v in row if k in used_fields] for row in rows]
             rt = [r + [(RAW_ID_NAME, idx)] for idx, r in enumerate(rt)]
@@ -106,40 +121,40 @@ def load_csv(fpath,mapping):
     return fname,rt
 
 def load_mapping(mfile):
-    '''Parse the mapping configure. Return the a dict with following structure:
+    '''Parse the mapping configure. Return the a list with following structure:
     [ 
-        { 
-            field_name_1: [field_type, default_value, vmap, source_field],
+        (
+            table_name,
+            source_csv,
+            (field_name_1, [field_type, default_value, vmap, source_field]),
             ... ...
-            field_name_n: [field_type, default_value, vmap, source_field],
-            "_s_name":  source_csv
-            "_t_name":  table_name
-        },
-        { 
-            field_name_1: [field_type, default_value, vmap, source_field],
+            (field_name_n, [field_type, default_value, vmap, source_field]),
+        ),
+        (
+            table_name,
+            source_csv,
+            (field_name_1, [field_type, default_value, vmap, source_field]),
             ... ...
-            field_name_n: [field_type, default_value, vmap, source_field],
-            "_s_name":  source_csv
-            "_t_name":  table_name
-        },
+            (field_name_n, [field_type, default_value, vmap, source_field]),
+        ),
     ]
     '''
     mapping=[]
     def pop_map(mp_row):
         t_name, tf_name, tf_type, tf_default, vmap, s_name, sf_name = [v.lower() for v in mp_row]
         if not tf_type:
-            print 'Error: The "Type" of the field "%s" not specified for table "%s".' % (tf_name, t_name or mapping[-1]['_t_name'])
+            print 'Error: The "Type" of the field "%s" not specified for table "%s".' % (tf_name, t_name or mapping[-1][0])
             sys.exit(0)
         if tf_type == 'int' and tf_default:
             try:int(tf_default)
             except:
-                print 'Error: The default value "%s" is not a valid integer for field "%s" of table "%s".' % (tf_default,tf_name, t_name or mapping[-1]['_t_name'])
+                print 'Error: The default value "%s" is not a valid integer for field "%s" of table "%s".' % (tf_default,tf_name, t_name or mapping[-1][0])
                 sys.exit(0)
         map_attr = [tf_type,tf_default,vmap,sf_name]
-        if t_name:
-            mapping.append({'_t_name':t_name, '_s_name':s_name, tf_name:map_attr})
+        if len(mapping) == 0 or (t_name and t_name != mapping[-1][0]):
+            mapping.extend([[t_name, s_name, [tf_name, map_attr]],])
         else:
-            mapping[-1].update({tf_name:map_attr})
+            mapping[-1].append([tf_name,map_attr])
     with open(mfile,'rb') as f:
         reader=UnicodeReader(f)
         reader.next() #Ignore the headers
@@ -167,18 +182,13 @@ def _m2m_table(ttable, fmp):
 def _mid_tables(mapping):
     mid_tbls = []
     for mp in mapping:
-        table_name=mp['_t_name']
-        m2m_fields = [(k,v) for k,v in mp.items() if (k not in ('_t_name','_s_name')) and v[0]=='m2m']
+        table_name=mp[0]
+        m2m_fields = [(k,v) for k,v in mp[2:] if v[0]=='m2m']
         mid_tbls.extend([_m2m_table(table_name, fmp) for _,fmp in m2m_fields])
     return mid_tbls
     
-@_trac_error
 def _parse_map(mp):
-    field_maps_dict = dict([(k,v) for k,v in mp.items() if k not in ('_t_name','_s_name')])
-    pk_field = [k for k,v in mp.items() if k not in ('_t_name','_s_name') and v[0]=='pk'][0]
-    pk_value = field_maps_dict.pop(pk_field)
-    field_maps = [(pk_field, pk_value)] + [pair for pair in field_maps_dict.items()] #We need to put pk field at the first place
-    return mp['_t_name'], mp['_s_name'], field_maps
+    return mp[0], mp[1], mp[2:]
     
 def resolve(raw_data, mapping):
     '''Resolve the raw data raw_data from CSV according to the configure mapping, and generate the clean data to be inserted into database. raw_data is a dict with the key being the CSV file name and the value being the data rows loaded from the CSV file. 
@@ -267,9 +277,7 @@ def resolve(raw_data, mapping):
                 
     def _retros_tbl_map(table_name,mapping):
         u'''return a structure as "[(fmp, csv), ..., (fmp, csv)]"'''
-        return sum([[(fmp,mp['_s_name']) for f,fmp in mp.items() \
-                    if (f not in ('_t_name','_s_name')) and fmp[2].startswith(table_name)] \
-                for mp in mapping],[])
+        return sum([[(fmp,mp[1]) for f,fmp in mp[2:] if fmp[2].startswith(table_name)] for mp in mapping],[])
                 
     def _parse_vmap(vmap,rval):
         tbl_mfield, _, cat = vmap.partition('+')
@@ -307,8 +315,8 @@ def resolve(raw_data, mapping):
         rt.update({table_name:resolved_data})
     
     for mp in mapping: # 2nd round resolving, generate M2M table
-        table_name=mp['_t_name']
-        m2m_fields = [(k,v) for k,v in mp.items() if (k not in ('_t_name','_s_name')) and v[0]=='m2m']
+        table_name=mp[0]
+        m2m_fields = [(k,v) for k,v in mp[2:] if v[0]=='m2m']
         if m2m_fields:
             for f,fmp in m2m_fields:
                 m2_ref_tbl, _, m2field = fmp[2].partition('.')
@@ -324,14 +332,31 @@ def resolve(raw_data, mapping):
                                 for row in rt[table_name]] #since the m2m mid table is resolved, we then remove the m2m field in the related table
             rt[table_name] = todb_data
     return dict([(tbl, [filter(lambda x:x[0]!=RAW_ID_NAME, r) for r in rows]) for tbl,rows in rt.items()]) #remove the fake field before reture
+
+def _gen_standard_sql(table,row1):
+    return u'INSERT INTO %s (%s) VALUES (%s)' % \
+                (table, ','.join([k for k,_ in row1]), ','.join([':c%s'%i for i,_ in enumerate(row1)]))
+def _gen_msaccess_sql(table,row1):
+    return u'INSERT INTO %s (%s) VALUES (%s)' % \
+                (table, ','.join([k for k,_ in row1]), ','.join(['?' for _ in row1]))
     
 def _gen_sql(table, row1):
-    return u'INSERT INTO %s (%s) VALUES (%s)' % \
-                (table, ','.join([k for k,_ in row1]), ','.join('?'*len(row1)))
+    return dict(zip(SUPPORTED_DBS, [_gen_standard_sql, _gen_standard_sql, _gen_msaccess_sql]))[args.db_dialect](table, row1)
     
-def _execute(table,rows,conn,cursor):
+def _execute(table,rows,conn,cursor):            
     sql = _gen_sql(table, rows[0])
-    cursor.executemany(sql,[[v for _,v in r] for r in rows])
+    if args.verbose:
+        print 'Running SQL:\n', sql, '\ndata:\n',rows[:1]
+    if args.batch_insert:
+        cursor.executemany(sql,[[v for _,v in r] for r in rows])
+    else:
+        for r in rows:
+            row = [v for _,v in r]
+            try:
+                cursor.execute(sql, row)
+            except:
+                print 'Error when inserting row: %s.' % row
+                sys.exit(0)
     conn.commit()
     
 def _print_stat(table, rows):
@@ -342,11 +367,13 @@ def _print_stat(table, rows):
     print '|'.join([" " + k.ljust(max(len(k),10)+1) for k,_ in rows[0]]) # Table Headers
     print '\n'.join(['|'.join([" " + (str(v)[:10]).ljust(max(len(k),10)+1) for k,v in row]) for row in rows[:2]]), '\n' # some rows
 
-#map between configure type and field type 
-DBTMAP = dict((
-        ('sqlite', dict((('pk', 'int'),('m2o','int'),('text','text'),('int','int'),('datetime','text'),('m2m','int')))),
-        ('oracle', dict((('pk', 'number'),('m2o','number'),('text','varchar2(256)'),('int','number'),('datetime','timestamp'),('m2m','number')))),
-)) 
+# map between configure type and field type
+# SUPPORTED_DBS = ['oracle','sqlite3','msaccess']
+DBTMAP = dict(zip(SUPPORTED_DBS, 
+    (dict((('pk', 'number'),('m2o','number'),('text','varchar2(256)'),('int','number'),('datetime','timestamp'),('m2m','number'))),
+    dict((('pk', 'int'),('m2o','int'),('text','text'),('int','int'),('datetime','text'),('m2m','int'))),
+    dict((('pk', 'LONG'),('m2o','LONG'),('text','VARCHAR'),('int','LONG'),('datetime','DATETIME'),('m2m','LONG'))),)))
+
     
 def _gen_ddl(tbl,field_defs):
     if not field_defs:return
@@ -355,19 +382,13 @@ def _gen_ddl(tbl,field_defs):
     return 'CREATE TABLE %s (%s)' %(tbl,fds)
     
 def gen_mid_ddl(mid_tbl, mid_id_fname, mid_m_fname, mid_m2_fname):
-    if args.sqlite_db:
-        return _gen_ddl(mid_tbl, map(lambda x:(x,DBTMAP['sqlite']['int']), [mid_id_fname, mid_m_fname, mid_m2_fname]))
-    else: #Oracle
-        return _gen_ddl(mid_tbl, map(lambda x:(x,DBTMAP['oracle']['int']), [mid_id_fname, mid_m_fname, mid_m2_fname]))
+    return _gen_ddl(mid_tbl, map(lambda x:(x,DBTMAP[args.db_dialect]['int']), [mid_id_fname, mid_m_fname, mid_m2_fname]))
        
 def _map_dbtype(otype):
-    if args.sqlite_db:
-        return DBTMAP['sqlite'][otype]
-    else:
-        return DBTMAP['oracle'][otype]
+    return DBTMAP[args.db_dialect][otype]
 
 def _gen_fdefs(tbl, row, mapping):
-    mps = filter(lambda x:x['_t_name']==tbl, mapping)
+    mps = filter(lambda x:x[0]==tbl, mapping)
     if mps:
         field_maps = dict(_parse_map(mps[0])[2])
         return [(f,_map_dbtype(field_maps[f][0])) for f,_ in row]
@@ -375,18 +396,39 @@ def _gen_fdefs(tbl, row, mapping):
 def gen_ddls(table_data, mapping):
     rt = [_gen_ddl(tbl, _gen_fdefs(tbl, rows[0], mapping)) for tbl, rows in table_data.items()]
     return filter(lambda x:x, rt) #remove blank element
+
+def _conn_oracle():
+    import cx_Oracle as oracle
+    os.environ["NLS_LANG"] = ".%s"%args.db_encoding.upper()
+    return oracle.connect(args.db_user, args.db_pwd, oracle.makedsn(args.db_host,args.db_port,args.db_sid))
+    
+def _conn_sqlite3():
+    import sqlite3
+    return sqlite3.connect(args.db_host)
+    
+def _conn_msaccess():
+    import pyodbc
+    return pyodbc.connect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=%s;" % args.db_host)
+    
+def _conn():
+    return dict(zip(SUPPORTED_DBS, [_conn_oracle, _conn_sqlite3, _conn_msaccess]))[args.db_dialect]()
     
 def dump_data(table_data,mapping):
     if args.execute:
         if args.verbose:print 'Connecting Database ...'
-        if args.sqlite_db:
-            conn = sqlite3.connect(args.sqlite_db)
-        else:
-            conn = oracle.connect(args.db_user, args.db_pwd, oracle.makedsn(args.db_host,args.db_port,args.db_sid))
+        conn = _conn()
         cs = conn.cursor()
         if args.create_table:
             ddls = gen_ddls(table_data, mapping) + [gen_mid_ddl(*ff) for ff in _mid_tables(mapping)]
-            for ddl in ddls:
+            for ddl in ddls: #Drop tables first
+                table_name = ddl.split()[2]
+                if args.verbose:print 'Droping Table:', table_name
+                try:
+                    cs.execute('DROP TABLE %s' % table_name)
+                except:#Simply ignore the error if the table is not there.
+                    if args.verbose:print 'Error when drop the table "%s"' % table_name
+            conn.commit()
+            for ddl in ddls: #Create Tables
                 if args.verbose:print 'Creating Table:\n', ddl
                 cs.execute(ddl)
             conn.commit()
