18d076107cdca549adf3b96b9738edc8ec390e36
johnwang507 <john.wang507@gmail.com>
2013-05-05 23:18:48 +0800

    ongoing

--- dataloader.clj/src/dataloader.clj
+++ dataloader.clj/src/dataloader.clj
@@ -1,7 +1,8 @@
 (ns dataloader
-  (:use '[clojure.tools.cli :only [cli]]
-        '[clojure.string :only [split split-lines]]))
-  
+  (:use [clojure.tools.cli :only [cli]]
+        [clojure.string :only [split]])
+  (:import [au.com.bytecode.opencsv.CSVReader]
+           [java.io InputStreamReader FileInputStream]))
 
 (defn get-args [args]
   "Parse cmdline arguments into "
@@ -9,25 +10,38 @@
   ["-h" "--help" "Show help" :flag true :default false]
   ["-w" "--raw_date_fmt" "The date format in the data file" 
     :parse-fn #(re-find #"" %) :default "%m/%d/%Y %I:%M:%S %p"]))
-
     
-(defn parse-cfgrw [[t_name, tf_name, tf_type, tf_default, vmap, s_name, sf_name]]
-  ())
+(defn parse-mp [rt [t-nm tf-nm tf-tp tf-dft vmp s-nm sf-nm]] 
+            (if (or (seq rt) (and (seq t-nm) (-> rt last first (= t-nm))))
+              (conj rt [t-nm, s-nm, [tf-nm, tf-tp,tf-dft,vmp,sf-nm]])
+              (conj (-> rt drop-last vec) (conj (last rt) [tf-nm, tf-tp,tf-dft,vmp,sf-nm]))))
 
-(defn load-mapping [mfile]
+(defn read-csvf [path encoding] (-> path FileInputStream. (InputStreamReader. encoding) CSVReader. .readAll))
+
+(defn load-mapping [mfile opts]
   "Parse the mapping configure. Return a vector with following structure:
     [ 
-      [table_name_1,source_csv_1
-        [field_name_1, [field_type, default_value, vmap, source_field]]
-        [field_name_2, [field_type, default_value, vmap, source_field]]
+      [table_name_1,source_csv_1 
+        [field_name_1, field_type, default_value, vmap, source_field]
           ...
-        ],
-        ... ...
+      ],
+      ... ...
     ]"
-  (reduce parse-cfgrw [] (-> mfile slurp split-lines )))
+  (reduce parse-mp [] (read-csvf mfile (:enc-cf opts))))
+  
+(defn used-sfields [snm mp] (->> (filter #(= snm (nth % 1)) mp)
+                                  first rest rest
+                                  (map #(-> % last keyword))
+                                  set))
 
-(defn load-csv [fpath mapping]
-  ())
+(defn load-csv [path mp opts]
+  (let [snm (-> path (split #"[/\\]") last (split #"\.") first)
+        usfs (used-sfields snm mp)
+        data (read-csvf path (:enc-data opts))
+        sfs (->> data first (map #(keyword %)))
+        srws (->> data rest (map #(list %1 %2) sfs))]
+    (map (fn [idx rw] (->> rw (filter (fn [[k _]] (sfs k))) vec (conj [:rwid idx]))) (range) srws)))
+        
 
 (defn resolve [raw-data mapping]
   ())
