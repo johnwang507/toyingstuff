82a0d299ff73f83888c0bfb903398c5058ab7da6
John Wang <john.wang507@gmail.com>
2013-07-15 11:17:44 +0800

    update

--- preloadrepo/trload.py
+++ preloadrepo/trload.py
@@ -1,39 +1,41 @@
 #!/usr/bin/env python
 
-import sys, os, subprocess as sbp
+import sys, os, shutil, subprocess as sbp
 
-arch_file = 'archfile'
-cmd = '''D:/tools/maven-3.0.5/bin/mvn
+_mvn='mvn'
+_farch_group = 'delme'
+_farch_id = _farch_group
+_arch_file = 'archfile'
+_cmd = '''%s
         archetype:generate
         -q
-        -DgroupId=delme
-        -DartifactId=delme
+        -DgroupId=%s
+        -DartifactId=%s
         -DarchetypeGroupId=%s
         -DarchetypeArtifactId=%s
         -DinteractiveMode=false
         '''
 
-
-
-# d:/tools/vn archetype:generate \
-# -DgroupId=org.sonatype.mavenbook \
-# -DartifactId=quickstart \
-# -Dversion=1.0-SNAPSHOT \
-
-# -DpackageName=org.sonatype.mavenbook \
-# -DarchetypeGroupId=org.apache.maven.archetypes \
-# -DarchetypeArtifactId=maven-archetype-quickstart \
-# -DarchetypeVersion=1.0 \
-# -DinteractiveMode=false
-
-
-
+def _clear():
+    os.chdir('..')
+    shutil.rmtree(_farch_id)
 
 def load(line):
-    cmd = (cmd % line.split(':')).split()
-    print 'Running cmd:' cmd
-    sbp.call()
-
+    atype_g, atype_a = map(lambda x:x.strip(),line.split(':'))
+    cmd = _cmd % (_mvn, _farch_group, _farch_id, atype_g, atype_a)
+    print 'Init archetype:', atype_g, atype_a
+    if sbp.call(cmd.split(), shell=True):
+        print 'Failed init archetype'
+        return 1
+    os.chdir(_farch_id)
+    if sbp.call([_mvn, '-q', 'dependency:resolve'], shell=True):
+        print 'Failed resolving dependencies.'
+        _clear()
+        return 1
+    if sbp.call([_mvn, '-q', 'dependency:sources'], shell=True):
+        print 'Cannot retrieve sources.'
+    _clear()
+    return 0
 
 if __name__ == '__main__':
     if len(sys.argv)>1:
@@ -41,5 +43,6 @@ if __name__ == '__main__':
     if not os.path.exists(arch_file):
         print 'file not exists: "%s"' % arch_file
         sys.exit(1)
-    with open(file,'r') as f:
+    with open(arch_file, 'r') as f:
         rts = [load(line) for line in f]
+        print 'Done. Total %d, %d failed.' %(len(rts), sum(rts))
