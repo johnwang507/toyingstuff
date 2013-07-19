1b5f86f1b1ccde7fd3001e75d9a577656757d10e
John Wang <john.wang507@gmail.com>
2013-07-19 11:58:39 +0800

    add pep381client process monitor

---
+++ pep381mon.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+import sys,os, time, subprocess as sbp
+
+def sync(i):
+    if i>1: time.sleep(60)
+    print 'Try', i, 'times ...'
+    lgidx = max(filter(lambda x:x.startswith('pep381run'),os.listdir('.'))).split('.')[0][9:]
+    try:lgidx = int(lgidx)
+    except:lgidx = 0
+    return i, sbp.call(['pep381run.py', 
+                    'pypi',
+                    '>pep381run%s.log'% (lgidx+i),
+                    ], shell=True)
+
+if __name__ == '__main__':
+    tries = int(sys.argv[1]) if len(sys.argv)>1 else 3
+    rt = [sync(i) for i in range(1, tries)]
+    print 'Try %d times, %d failed.' % (len(rt), sum(zip(*rt)[1]))
