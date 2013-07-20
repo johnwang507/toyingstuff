375e66b3c7ffcd0f65ef5a0a61c76ec1aec7c1c7
John Wang <john.wang507@gmail.com>
2013-07-20 17:36:28 +0800

    update

--- datawell.clj/project.clj
+++ datawell.clj/project.clj
@@ -1,6 +1,9 @@
 (defproject datawell.clj "0.1.0-SNAPSHOT"
-  :description "A tool emitting data records. It can be used to generate test data."
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+    :description "A tool emitting data records. It can be used to generate test data."
+    :url "https://github.com/johnwang507/toyingstuff/tree/master/datawell.clj"
+    :license {:name "Eclipse Public License"
+    :url "http://www.eclipse.org/legal/epl-v10.html"}
+    :dependencies [[org.clojure/clojure "1.5.1"]
+    							[org.clojure/tools.cli "0.2.1"]]
+    :main datawell.main
+    							)
