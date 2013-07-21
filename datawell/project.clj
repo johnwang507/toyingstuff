8a979726d5d73b04edfba67631ac04889153e4df
John Wang <john.wang507@gmail.com>
2013-07-21 22:07:46 +0800

    remove python style clojure syntax demo

--- datawell/project.clj
+++ datawell/project.clj
@@ -1,8 +1,8 @@
 (defproject datawell "0.1.0-SNAPSHOT"
-    :description "A tool emitting data records. It can be used to generate test data."
-    :url "https://github.com/johnwang507/toyingstuff/tree/master/datawell.clj"
-    :license {:name "Eclipse Public License"
-    :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :main datawell.main 
-    :dependencies [[org.clojure/clojure "1.5.1"]
-    							[org.clojure/tools.cli "0.2.2"]])
+  :description "A tool emitting data records. It can be used to generate test data."
+  :url "https://github.com/johnwang507/toyingstuff/tree/master/datawell.clj"
+  :license {:name "Eclipse Public License"
+  :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main datawell.main 
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.cli "0.2.2"]])
