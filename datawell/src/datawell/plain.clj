80be042abe2d24fc8933a0a5bcffc7e236b1ad3c
John Wang <john.wang507@gmail.com>
2013-07-22 21:30:11 +0800

    add test

--- datawell/src/datawell/plain.clj
+++ datawell/src/datawell/plain.clj
@@ -1,23 +1,23 @@
 (def pint #(Integer. %))
 
 (defn ex-args []
-  '(["-n" "--col_number" "The number of column in a record"
+  '(["-n" "--col-number" "The number of column in a record"
     :default 5 :parse-fn pint]
-    ["-w" "--col_width" "The width in number of character of the column" 
+    ["-w" "--col-width" "The width in number of character of the column" 
     :default 15 :parse-fn pint]
-    ["-f" "--[no-]fixed_len" "The length of every column is random within the limit of col_width"
+    ["-f" "--[no-]fixed-len" "The length of every column is random within the limit of col_width"
     :default false]
     ["-a" "--alphabet" "If presented, the random string is generated only from alphabet(no special char e.g., + )"
     :default false]))
 
-(defn gen-col [seeds maxlen fixed_len]
+(defn gen-col [seeds maxlen fixed-len]
   (let [len (if fixed_len
     maxlen
     (inc (rand-int maxlen)))]
   (apply str (repeatedly len #(->> seeds (map char) rand-nth)))))
 
-(defn gen [idx pre {:keys [col_number col_width fixed_len alphabet]} args]
+(defn gen [idx pre {:keys [col-number col-width fixed-len alphabet]} args]
   (let [seeds (if alphabet
     (concat (range 65 91) (range 97 123))
     (range 32 127))]
-  (repeatedly col_number #(gen-col seeds col_width fixed_len))))
+  (repeatedly col_number #(gen-col seeds col-width fixed-len))))
