6ba14b4a013e22a6383213a2c89d367de345ff46
John Wang <john.wang507@gmail.com>
2013-08-04 18:47:09 +0800

    add service

--- datawell/test/datawell/main_test.clj
+++ datawell/test/datawell/main_test.clj
@@ -1,7 +1,17 @@
 (ns datawell.main-test
-  (:require [datawell.plain :refer :all])
+  (:require [datawell.main :as main])
   (:use midje.sweet))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn- all-alp? [x] (every? (->> (concat (range 65 91) (range 97 123)) (map char) set) x))
+
+(facts "Generate a record"
+  (let [arow (main/spout 0 0 {:colnum 9 :colsize 9 :fixedlen false :alphabet false} 0)
+    lens (map count arow)]
+    lens => (nine-of #(< % 10))
+    (apply not= lens) => true
+    (every? all-alp? arow) => false)
+  (let [arow (main/spout 0 0 {:colnum 9 :colsize 9 :fixedlen true :alphabet true} 0)
+    lens (map count arow)]
+    lens => (nine-of 9)
+    (apply not= arow) => true
+    (every? all-alp? arow) => true))
