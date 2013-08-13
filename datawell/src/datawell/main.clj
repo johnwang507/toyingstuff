620709376cd366cb22a1c1150b4c66541616b41c
John Wang <john.wang507@gmail.com>
2013-08-13 23:22:06 +0800

    only -main func left

--- datawell/src/datawell/main.clj
+++ datawell/src/datawell/main.clj
@@ -5,63 +5,64 @@
         [ring.adapter.jetty :only [run-jetty]]
         [ring.util.response :only [response]]
         [clojure.contrib.server-socket])
-  (:import [java.io BufferedReader InputStreamReader OutputStreamWriter])
+  (:import [java.io PrintWriter])
   (:gen-class))
-
-(def ^:private roof (atom -1))
-(def ^:private counter (atom 0))
 
 (defn- gen-col
   "Compose a string with no more than *maxlen* long, by randomly picking characters from the *seeds*.
-  If *fixedlen* is true, the strings generated every times will all be *maxlen* long, 
-  otherwise their length are random. The *seeds* is a sequence of Integers corresponding to ASCII characters."
+  If *fixedlen* is true, all strings generated will be of the same *maxlen* long, 
+  otherwise their length are random. The *seeds* is a sequence of Integers of ASCII characters."
   [seeds maxlen fixedlen]
   (let [len (if fixedlen maxlen (inc (rand-int maxlen)))]
   (apply str (repeatedly len #(->> seeds (map char) rand-nth)))))
 
-(defn spout [{:keys [colnum colsize fixedlen alphabet limit]}]
-  (let [seeds (if alphabet (concat (range 65 91) (range 97 123)) (range 32 127))]
-      (repeatedly colnum #(gen-col seeds colsize fixedlen))))
+(defn spout [{:keys [colnum colength fixedlen words seperator]}]
+  (let [base (if words (range 97 123) (range 32 127))
+        sep (-> seperator first int)
+        seeds (remove #(= sep %) base)]
+      (join seperator (repeatedly colnum #(gen-col seeds colength fixedlen)))))
 
-(defn limit-spout [opts]
-  (if (or (< roof 0) (< counter roof))
-      (do (swap! counter inc)
-          (spout opts))
-      (System/exit 0)))
+(defn emit [opts fn-send fn-clean]
+  (let [{:keys [maxnum interval]} opts]
+    (loop [x 0N]
+      (if (< x maxnum)
+          (do (-> opts spout fn-send) (Thread/sleep interval) (recur (inc x)))
+          (if (ifn? fn-clean) (fn-clean))))))
 
-(defn svc-http [opts args]
-    (run-jetty (fn [request] (response (limit-spout opts))) {:port (:port opts)}))
+(defn svc-http [opts]
+    (run-jetty (fn [request] (response (spout opts))) {:port (:port opts)}))
 
-(defn svc-stdout [opts args] (repeatedly #(println (limit-spout opts))))
+(def pemit #(emit % println nil))
 
-(defn svc-socket [opts args]
-  (run-socket handler {:port (:port opts)})) ;go here 
+(defn svc-stdout [opts] (pemit opts))
 
-(defn svc-zeromq [opts args] 1);TODO 
+(defn svc-socket [opts]
+  (create-server (:port opts) (fn [_ out] (binding [*out* (PrintWriter. out)] (pemit opts)))))
 
-(def ifc-types [1 #'svc-stdout 2 #'svc-http 3 #'svc-socket 4 #'svc-zeromq])
+(def svc-type [1 #'svc-stdout 2 #'svc-http 3 #'svc-socket])
 
 (defn- pargs [args]
-  (let [pint #(Long. %)
-    type-opts (->> ifc-types (take-nth 2) (join ", "))
-    type-nms (->> ifc-types rest (take-nth 2) (map #(-> % meta :name name (.substring 4))) (join ", "))]
+  (let [pint #(bigint %)
+    type-opts (->> svc-type (take-nth 2) (join ", "))
+    type-nms (->> svc-type rest (take-nth 2) (map #(-> % meta :name name (.substring 4))) (join ", "))]
   (cli args
     ["-h" "--help" "Show this help." :flag true :default false]
     ["-t" "--type" (str "The type of the service. Avaliable: " type-opts ", standing for " type-nms ".") :parse-fn pint :default 1]
     ["-p" "--port" "The port to bind" :parse-fn pint :default 9876]
     ["-i" "--interval" "The time interval between two consecutive records being generated, in millisecond. Ignored if the type of service is pull-style(e.g., http) instead of push-style(e.g., stdout, socket)." :parse-fn pint :default 200]
-    ["-l" "--limit" "How many records to be generated before the service stop. a negative value makes the generating never stop." :parse-fn pint :default 5]
+    ["-m" "--maxnum" "The max number of records to be generated in push-style server(e.g., stdout, socket). After that the server will stop pushing records" :parse-fn pint :default 5]
     ["-n" "--colnum" "How many columns in a row." :parse-fn pint :default 9]
-    ["-s" "--colsize" "How many characters can a column hold, e.g., the column size." :parse-fn pint :default 15]
+    ["-l" "--colength" "How many characters can a column hold, e.g., the column size." :parse-fn pint :default 15]
     ["-f" "--fixedlen" "Should the size of every column be the same." :flag true :default true]
-    ["-a" "--allchars" "Use all visiable characters to generate data. Otherwise only alphabet will be used." :flag true :default true])))
+    ["-s" "--seperator" "What character will be the seperator for columns. This character will not be in characters seeds." :default "\t"]
+    ["-w" "--words" "Use only lowcase alphabet character as seeds to generate random string. Otherwise all visiable characters will be used." :flag true :default true])))
 
 (defn -main [& args]
   (let [[opts args banner] (pargs args)]
     (when (:help opts)
       (println banner)
       (System/exit 0))
-    (let [svc (->> opts :type (.indexOf ifc-types) inc (nth ifc-types))]
-      (reset! roof (:limit opts))
+    (let [svc (->> opts :type (.indexOf svc-type) inc (nth svc-type))]
+      (reset! roof (:maxnum opts))
       (meta svc))))
 
