112cd8e4db02fa2c6fa3013a637d17fff4256e6f
John Wang <john.wang507@gmail.com>
2013-08-20 21:32:01 +0800

    done

--- datawell/src/datawell/main.clj
+++ datawell/src/datawell/main.clj
@@ -16,53 +16,46 @@
   (let [len (if fixedlen maxlen (inc (rand-int maxlen)))]
   (apply str (repeatedly len #(->> seeds (map char) rand-nth)))))
 
-(defn spout [{:keys [colnum colength fixedlen words seperator]}]
+(defn gen-row [{:keys [colnum colength fixedlen words seperator]}]
   (let [base (if words (range 97 123) (range 32 127))
         sep (-> seperator first int)
         seeds (remove #(= sep %) base)]
       (join seperator (repeatedly colnum #(gen-col seeds colength fixedlen)))))
 
-(defn emit [opts fn-send fn-clean]
+(defn print-rows [opts]
   (let [{:keys [maxnum interval]} opts]
     (loop [x 0N]
       (if (< x maxnum)
-          (do (-> opts spout fn-send) (Thread/sleep interval) (recur (inc x)))
-          (if (ifn? fn-clean) (fn-clean))))))
+          (do (-> opts gen-row println) (Thread/sleep interval) (recur (inc x)))))))
 
 (defn svc-http [opts]
-    (run-jetty (fn [request] (response (spout opts))) {:port (:port opts)}))
+    (run-jetty (fn [request] (response (gen-row opts))) {:port (:port opts)}))
 
-(def pemit #(emit % println nil))
-
-(defn svc-stdout [opts] (pemit opts))
+(defn svc-stdout [opts] (print-rows opts))
 
 (defn svc-socket [opts]
-  (create-server (:port opts) (fn [_ out] (binding [*out* (PrintWriter. out)] (pemit opts)))))
+  (create-server (:port opts) (fn [_ out] (binding [*out* (PrintWriter. out)] (print-rows opts)))))
 
-(def svc-type [1 #'svc-stdout 2 #'svc-http 3 #'svc-socket])
+(def svc-type ["stdout" "http" "socket"])
 
 (defn- pargs [args]
-  (let [pint #(bigint %)
-    type-opts (->> svc-type (take-nth 2) (join ", "))
-    type-nms (->> svc-type rest (take-nth 2) (map #(-> % meta :name name (.substring 4))) (join ", "))]
-  (cli args
-    ["-h" "--help" "Show this help." :flag true :default false]
-    ["-t" "--type" (str "The type of the service. Avaliable: " type-opts ", standing for " type-nms ".") :parse-fn pint :default 1]
-    ["-p" "--port" "The port to bind" :parse-fn pint :default 9876]
-    ["-i" "--interval" "The time interval between two consecutive records being generated, in millisecond. Ignored if the type of service is pull-style(e.g., http) instead of push-style(e.g., stdout, socket)." :parse-fn pint :default 200]
-    ["-m" "--maxnum" "The max number of records to be generated in push-style server(e.g., stdout, socket). After that the server will stop pushing records" :parse-fn pint :default 5]
-    ["-n" "--colnum" "How many columns in a row." :parse-fn pint :default 9]
-    ["-l" "--colength" "How many characters can a column hold, e.g., the column size." :parse-fn pint :default 15]
-    ["-f" "--fixedlen" "Should the size of every column be the same." :flag true :default true]
-    ["-s" "--seperator" "What character will be the seperator for columns. This character will not be in characters seeds." :default "\t"]
-    ["-w" "--words" "Use only lowcase alphabet character as seeds to generate random string. Otherwise all visiable characters will be used." :flag true :default true])))
+  (let [pint #(bigint %)]
+    (cli args
+      ["-h" "--help" "Show this help." :flag true :default false]
+      ["-t" "--type" (str "The type of the service. Avaliable: " svc-type) :default (first svc-type)]
+      ["-p" "--port" "The port to bind, used in network service(e.g., http, socket)" :parse-fn pint :default 9876]
+      ["-i" "--interval" "The time interval between two consecutive records being generated, in millisecond. Ignored if the type of service is pull-style(e.g., http) instead of push-style(e.g., stdout, socket)." :parse-fn pint :default 200]
+      ["-m" "--maxnum" "The max number of records to be generated in push-style server(e.g., stdout, socket). After that the server will stop pushing records" :parse-fn pint :default 5]
+      ["-n" "--colnum" "How many columns in a row." :parse-fn pint :default 9]
+      ["-l" "--colength" "How many characters can a column hold, e.g., the column size." :parse-fn pint :default 15]
+      ["-f" "--fixedlen" "Should the size of every column be the same." :flag true :default true]
+      ["-s" "--seperator" "What character will be the seperator for columns. This character will not be in characters seeds." :default "\t"]
+      ["-w" "--words" "Use only lowcase alphabet character as seeds to generate random string. Otherwise all visiable characters will be used." :flag true :default true])))
 
 (defn -main [& args]
   (let [[opts args banner] (pargs args)]
-    (when (:help opts)
+    (if (:help opts)
       (println banner)
-      (System/exit 0))
-    (let [svc (->> opts :type (.indexOf svc-type) inc (nth svc-type))]
-      (reset! roof (:maxnum opts))
-      (meta svc))))
+      (let [svc (->> opts :type (str "svc-") symbol (ns-resolve 'datawell.main))]
+        (svc opts)))))
 
--- datawell/test/datawell/main_test.clj
+++ datawell/test/datawell/main_test.clj
@@ -1,17 +1,20 @@
 (ns datawell.main-test
   (:require [datawell.main :as main])
-  (:use midje.sweet))
+  (:use midje.sweet
+        [clojure.string :only [split]]))
 
-(defn- all-alp? [x] (every? (->> (concat (range 65 91) (range 97 123)) (map char) set) x))
+(defn- alllowcase? [x] (every? (->> (range 97 123) (map char) set) x))
 
 (facts "Generate a record"
-  (let [arow (main/spout 0 0 {:colnum 9 :colsize 9 :fixedlen false :alphabet false} 0)
-    lens (map count arow)]
+  (let [opts {:colnum 9 :colength 9 :fixedlen false :words false :seperator "\t"}
+    col-seq (-> opts main/gen-row (split #"\t"))
+    lens (map count col-seq)]
     lens => (nine-of #(< % 10))
     (apply not= lens) => true
-    (every? all-alp? arow) => false)
-  (let [arow (main/spout 0 0 {:colnum 9 :colsize 9 :fixedlen true :alphabet true} 0)
-    lens (map count arow)]
+    (every? alllowcase? col-seq) => false)
+  (let [opts {:colnum 9 :colength 9 :fixedlen true :words true :seperator "\t"}
+    col-seq (-> opts main/gen-row (split #"\t"))
+    lens (map count col-seq)]
     lens => (nine-of 9)
-    (apply not= arow) => true
-    (every? all-alp? arow) => true))
+    (apply not= col-seq) => true
+    (every? alllowcase? col-seq) => true))
