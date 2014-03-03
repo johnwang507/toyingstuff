63804f444ad157c949b4a318ded9581600a0ce4e
John Wang <john.wang507@gmail.com>
2014-03-03 11:58:48 +0800

    dant beta

--- dant.py
+++ dant.py
@@ -8,12 +8,15 @@ import sys, os, signal, argparse, bs4, requests, urllib, codecs, traceback
 SITES_SHORTCUTS = (
     ('g', ('g_parser', 'g_filter', 'g_roller', "http://www.google.com.hk", "/search?q=" )), # Google
     ('b', ('b_parser', 'b_filter', 'b_roller', "http://cn.bing.com",       "/search?q=" )), # Bing
-    ('d', ('d_parser', 'd_filter', 'd_roller', "http://www.baidu.com",     "/#wd="      )), # Baidu
+    ('d', ('d_parser', 'd_filter', 'd_roller', "http://www.baidu.com",     "/s?wd="      )), # Baidu
     )
 
 PICKED_DOM_ELE = (('h', ('html', lambda x:x)), ('b', ('body', lambda x:x.body)), ('t', ('all text', lambda x:x.text)))
 
 MEDIA_SUFFIX = ['js', 'css', 'jpeg', 'jpg', 'png', 'bmp', 'gif', 'mp3', 'mp4', 'zip', 'tgz', 'gz', 'rar', ]
+
+# Small utility functions
+listlen = lambda x:len(x) if x else 0
 
 def get_args():
     parser = argparse.ArgumentParser(description='A vanilla crawler which collect short text from search engine',
@@ -54,73 +57,80 @@ def get_args():
 
 def output(doc):
     _data = unicode(doc)
-    CTX.FILE_IDX = CTX.FILE_IDX+1
-    CTX.BYTES = CTX.BYTES + len(_data)
-    if not CTX.ARGS.output_to:
-        print _data
+    CTX.file_idx = CTX.file_idx+1
+    CTX.all_bytes = CTX.all_bytes + len(_data)
+    if not CTX.args.output_to:
+        print _data.encode('unicode_escape')
     else:
-        with codecs.open(os.path.join(CTX.ARGS.output_to, CTX.FILE_IDX), 'w', CTX.ARGS.encoding) as f:
+        with codecs.open(os.path.join(CTX.args.output_to, str(CTX.file_idx)), 'w', CTX.args.encoding) as f:
             f.write(_data)
 
 def mk_soup(link): # todo: should return real url(redirected if any) and soup
     if not link:return None,None
     trk, _, tail = link.rpartition('.')
     if trk and (tail.lower() in MEDIA_SUFFIX): # Ignore media type
-        if CTX.ARGS.verbose:print 'Ignore media link', link
+        if CTX.args.verbose:print 'Ignore media link', link
         return None,None
     try:
-        if CTX.ARGS.verbose: print 'loading page:', link
-        ctype = requests.head(link, timeout=CTX.ARGS.timeout).headers['content-type'].split(';')[0]
+        if CTX.args.verbose: print 'loading page:', link
+        heads = requests.head(link, timeout=CTX.args.timeout)
+        ctype = heads.headers['content-type'].split(';')[0]
         if ctype.lower() in ('text/html', 'text/plain'): # Some links look not like rich media, but still we need to check.
-            response = requests.get(link, timeout=CTX.ARGS.timeout)
+            response = requests.get(link, timeout=CTX.args.timeout)
             soup = None if (response.status_code != requests.codes.ok) else bs4.BeautifulSoup(response.text)
             return soup, response.url
         else:
-            if CTX.ARGS.verbose: print ctype, 'content ignored.'
+            if CTX.args.verbose: print ctype, 'content ignored.'
             if trk: MEDIA_SUFFIX.append(tail.lower()) # Add more trailling suffix to filter out the media links for following process.
     except requests.exceptions.Timeout:
-        if CTX.ARGS.verbose: print 'Request timeout on', link
+        if CTX.args.verbose: print 'Request timeout on', link
     except:
-        if CTX.ARGS.verbose: traceback.print_exc()
+        if CTX.args.verbose: traceback.print_exc()
     return None,None
 
+# Google
 def g_parser(soup):
-    return sect.h3.a.get('href', None), sect.h3.next_sibling.find('span','st').text
+    return soup.h3.a.get('href', None), soup.h3.next_sibling.find('span','st').text
 def g_filter(soup):
     return soup.find_all('li','g')
 def g_roller(soup, curr_pidx):
     return soup.find('table', {"id": "nav"}).tr.contents[-1].a['href']
 
+ # Bing
 def b_parser(soup):
-    raise Exception('Not implemented yet.')
+    return soup.h3.a.get('href', None), soup.find('div', 'sa_mc').p.text
 def b_filter(soup):
-    raise Exception('Not implemented yet.')
+    return soup.find('div',{'id':'results'}).find_all('li', 'sa_wr')
 def b_roller(soup, curr_pidx):
-    raise Exception('Not implemented yet.')
+    return soup.find('div','sb_pag').select('li > a.sb_pagN')
 
+ # Baidu
 def d_parser(soup):
-    return sect.h3.a.get('href', None), sect.h3.next_sibling.text
+    return soup.h3.a.get('href', None), soup.h3.next_sibling.text
 def d_filter(soup):
     return soup.find_all('table','result')
 def d_roller(soup, curr_pidx):
     return soup.find('p', {"id": "page"}).find('a','n')['href']
 
 def parse_se_page(soup, sect_parser, sect_filter):
-    import pdb;pdb.set_trace()
     def psect(sect):
         try:
             return sect_parser(sect) 
         except:
+            if CTX.args.verbose:print traceback.print_exc()
             return None
     sections = sect_filter(soup)
+    if CTX.args.verbose: print listlen(sections), 'search results found.'
     sects = sections and [psect(sc) for sc in sections]
-    return sects and [sect for sect in sects if sect]
+    rt = sects and [sect for sect in sects if sect]
+    if CTX.args.verbose: print listlen(rt), 'search result parsed.'
+    return rt
 
 def roll_se_page(soup, roller, curr_pidx):
     try:
         return roller(soup, curr_pidx)
     except Exception as exp:
-        if CTX.ARGS.verbose: print 'Roll to last page or there is an error: ', exp
+        if CTX.args.verbose: print 'Roll to last page or there is an error: ', exp
 
 def mk_flink(link, plink):
     if not link or link.startswith('#'):return None
@@ -129,8 +139,8 @@ def mk_flink(link, plink):
 def follow(link, level=0):
     soup, realink = mk_soup(link)
     if not soup:return
-    output(dict(PICKED_DOM_ELE)[CTX.ARGS.picked_element][1](soup))
-    if level >= CTX.ARGS.depth: return
+    output(dict(PICKED_DOM_ELE)[CTX.args.picked_element][1](soup))
+    if level >= CTX.args.depth: return
     [follow(mk_flink(anch.get('href', None), realink), level+1) for anch in soup.find_all('a')]
 
 def roll_search(url, sparser, sfilter, roller, curr_pidx=1):
@@ -138,16 +148,16 @@ def roll_search(url, sparser, sfilter, roller, curr_pidx=1):
     if not soup:return
     for link, doc in parse_se_page(soup, sparser, sfilter):
         output(doc)
-        if link and CTX.ARGS.depth > 0:
+        if link and CTX.args.depth > 0:
             follow(mk_flink(link, realink), 1)
-    if curr_pidx < CTX.ARGS.roll_times:
+    if curr_pidx < CTX.args.roll_times:
         next_link = roll_se_page(soup, roller, curr_pidx)
-        return next_link and roll_search(mk_flink(next_link, realink), sparser, roller, curr_pidx+1)
+        return next_link and roll_search(mk_flink(next_link, realink), sparser, sfilter, roller, curr_pidx+1)
 
 def _main():
-    if len(CTX.ARGS.site) < 3: # Start from searching
-        sparser, sfilter, roller, site_url, spath = dict(SITES_SHORTCUTS)[CTX.ARGS.site]
-        url = ''.join([site_url, spath, urllib.quote(' '.join(CTX.ARGS.keywords))])
+    if len(CTX.args.site) < 3: # Start from searching
+        sparser, sfilter, roller, site_url, spath = dict(SITES_SHORTCUTS)[CTX.args.site]
+        url = ''.join([site_url, spath, urllib.quote(' '.join(CTX.args.keywords))])
         return roll_search(url, eval(sparser),eval(sfilter), eval(roller))
     else: # General crawler here.
         pass
@@ -155,9 +165,9 @@ def _main():
 if __name__ == '__main__':
     global CTX
     class CTX:
-        ARGS, BYTES, FILE_IDX = get_args(), 0, 0
+        args, all_bytes, file_idx = get_args(), 0, 0
     signal.signal(signal.SIGINT, lambda x,y:sys.exit(0))
     _main()
-    print 'Done. Total %s files, %s bytes' % (CTX.FILE_IDX, '{:,}'.format(CTX.BYTES))
+    print 'Done. Total %s files, %s bytes' % (CTX.file_idx, '{:,}'.format(CTX.all_bytes))
 
     
